66923bbd04bfc005884c60a93f34c9d4c23dcd72
Mattwhited <matthewwhited@hotmail.com>
2025-02-02 15:20:16 -0600

    A bunch of stuff.
    I am following a tutorial because I learn better
    by practicing and getting reps in.
    
    I will create an etch-a-sketch project from scratch
    later on...

--- java.js
+++ java.js
@@ -1,5 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+    boardSize(12)
+    console.log('farts')
+})
 
-let divBox = document.createElement("div");
+function boardSize(size) {
+    let main = document.querySelector(".main");
+    main.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    main.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-document.body.appendChild(divBox);
+    let numDivs = size * size;
 
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        main.insertAdjacentElement("beforeend", div);
+    }
+}
